befa4282b34c3b9ba2d60ed1330bdc57e8db252b
Richard Slater <richard.slater@ensono.com>
2026-07-06 10:36:42 +0100

    chore: add commands to setup new build

---
+++ scripts/windows/Install-OneTimePackages.ps1
@@ -0,0 +1,13 @@
+winget install --source winget --id junegunn.fzf --exact
+winget install --source winget --id BurntSushi.ripgrep.GNU --exact
+winget install --source winget --id DEVCOM.Lua --exact
+winget install --source winget --id JesseDuffield.lazygit --exact
+winget install --source winget --id sharkdp.fd --exact
+
+winget install --source winget --id ImageMagick.ImageMagick --exact
+winget install --source winget --id ArtifexSoftware.GhostScript --exact
+winget install --source winget --id MiKTeX.MiKTeX --exact
+
+npm install -g neovim
+npm install -g @mermaid-js/mermaid-cli
+
